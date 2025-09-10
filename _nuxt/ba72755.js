c2b1ef3e89308629c91c4d018b03f7e765c0323c
kazudayo <108916507+kazudayo@users.noreply.github.com>
2025-09-10 16:25:02 +0900

    fix

---
+++ _nuxt/ba72755.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{550:function(n,t,e){"use strict";e.r(t);var l=e(528),o={layout:"menuItem"},c=e(6),component=Object(c.a)(o,(function(){return(0,this._self._c)(l.a)}),[],!1,null,null,null);t.default=component.exports}}]);
